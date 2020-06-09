42b0efb98690aa106daafaa94042b676d550ae0b
stud <super1742720898@gmail.com>
2020-06-09 16:46:06 +0800

    2.1

--- pages/market/market.js
+++ pages/market/market.js
@@ -1,31 +1,76 @@
+const app = getApp()
 Page({
     data: {
-        categories: [{
-            name: '匕首',
-        },{
-            name: '手枪'
-        },{
-            name: '步枪'
-        },{
-            name: '冲锋枪'
-        },{
-            name: '霰弹枪'
-        },{
-            name: '机枪'
-        },{
-            name: '手套'
-        }],
-        currentMenuIndex: 0,//当前选中的项
-        categoryInfo: {}
+        StatusBar: app.globalData.StatusBar,
+        CustomBar: app.globalData.CustomBar,
+        Custom: app.globalData.Custom,
+        TabCur: 0,
+        MainCur: 0,
+        VerticalNavTop: 0,
+        list: [],
+        load: true
     },
-    changeCategory: function (event) {
-        // console.log(event);
-        var index = event.currentTarget.dataset.index;
-        this.setData({
-            currentMenuIndex: index
+    onLoad() {
+        wx.showLoading({
+            title: '加载中...',
+            mask: true
         });
+        let list = [{}];
+        for (let i = 0; i < 7; i++) {
+            list[i] = {};
+            list[i].id = i;
+        }
+        list[0].name = '匕首'
+        list[1].name = '手枪'
+        list[2].name = '步枪'
+        list[3].name = '冲锋枪'
+        list[4].name = '霰弹枪'
+        list[5].name = '机枪'
+        list[6].name = '手套'
+        this.setData({
+            list: list,
+            listCur: list[0]
+        })
     },
-    onLoad: function (options) {
-
+    onReady() {
+        wx.hideLoading()
+    },
+    tabSelect(e) {
+        this.setData({
+            TabCur: e.currentTarget.dataset.id,
+            MainCur: e.currentTarget.dataset.id,
+            VerticalNavTop: (e.currentTarget.dataset.id - 1) * 50
+        })
+    },
+    VerticalMain(e) {
+        let that = this;
+        let list = this.data.list;
+        let tabHeight = 0;
+        if (this.data.load) {
+            for (let i = 0; i < list.length; i++) {
+                let view = wx.createSelectorQuery().select("#main-" + list[i].id);
+                view.fields({
+                    size: true
+                }, data => {
+                    list[i].top = tabHeight;
+                    tabHeight = tabHeight + data.height;
+                    list[i].bottom = tabHeight;
+                }).exec();
+            }
+            that.setData({
+                load: false,
+                list: list
+            })
+        }
+        let scrollTop = e.detail.scrollTop + 20;
+        for (let i = 0; i < list.length; i++) {
+            if (scrollTop > list[i].top && scrollTop < list[i].bottom) {
+                that.setData({
+                    VerticalNavTop: (list[i].id - 1) * 50,
+                    TabCur: list[i].id
+                })
+                return false
+            }
+        }
     }
-});
+})
